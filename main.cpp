aa0b39160218c25be1020c9d227490eee7e89a8e
Seominchae0104 <smc2649083@ewha.ac.kr>
2026-09-04 01:44:35 +0000

    final commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <iostream>
 
 int main()
 {
-    int NumberOfStudent{31};
-    std::cout << NumberOfStudent << std::end1;
+    int score{};
+    int grade{};
 
-    return 0;
+    std::cin >> score;
+    switch (score/10)
+    {
+        case 10:
+        case 9: grade = 'A'; break;
+        case 8: grade = 'B'; break;
+        case 7: grade = 'C'; break;
+        case 6: grade = 'D'; break;
+        default: grade = 'F';
+
+
+    }
+    std::cout << score << "," <<grade std::end1
+    // int numberOfStudent{31};
+
+
+
+
+
+    // (numberOfStudent == 30 )? (std::cout) std::cout << "changed\n";
+
+    // if (numberOfStudent == 30) std::cout << "default\n";
+
+    // else std::cout << "changed\n";
+
+    
+
+
+    // if (numberOfStudent > 30) std::cout << "increased\n";
+    // else if (numberOfStudent < 30) std::cout << "decreased\n";
+    // else std::cout << "default\n";
+
+
+
+
+
+    std::cout << NumberOfStudent++ << std::end1;
+    std::cout << NumberOfStudent << std::end1;
+    std::cout << ++NumberOfStudent << std::end1;
+
+
+    //std::cin >> NumberOfStudent
+    // std::cout << "number of students: ";
+    // std::cin >> NumberOfStudent;
+    // std::cout << NumberOfStudent << std::end1;
+
+
+
+    // std::cout << NumberOfStudent << std::end1;
+    // std::cout << sizeof(NumberOfStudent) << std::end1;
+    // std::cout << typeid(NumberOfStudent) << std::end1;
+    // std::cout <<typeid(static_cast<double>(NumberOfStudent));
+    // return 0;
 }
